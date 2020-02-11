bdc20799649fb8104639a43f1cef12832cb6d371
Uzor Mercy <mercy4hope@gmail.com>
2020-02-11 20:44:24 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const navScrol = document.querySelector('.navbar');
+window.addEventListener('scroll', (event)=>{
+    if(window.scrollY > 475){
+        navScrol.classList.add('navbar__fixed');
+        console.log(window.scrollY);
+    }
+})
