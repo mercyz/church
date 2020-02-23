7fb8b5b1b626c99c17a790626e759210b901ee5d
Uzor Mercy <mercy4hope@gmail.com>
2020-02-23 07:14:02 +0100

    fixed the sticky navigation bar on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,6 @@ const navScrol = document.querySelector('.navbar');
 
 window.addEventListener('scroll', (event)=>{
     if(window.scrollY > 475){
-        // navScrol.classList.add('navbar__fixed');
+        navScrol.classList.add('navbar__fixed');
     }
 })
